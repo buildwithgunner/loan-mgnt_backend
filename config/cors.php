4f5e8d3afbf8d430ccea60c7fbe9696145daf681
Almightymayor <danielmoses849@gmail.com>
2026-05-10 09:31:21 +0200

    Update CORS config for Vercel deployment

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173'), 'https://frontend-alpha-livid-56.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
